deab9d1cc0c4e5f75335dfd724c4bd19d8a03ca4
Maycon Mendes <maycon.mendes@senior.com.br>
2023-01-20 21:28:41 -0300

    [FEAT] - Criação, estilização e integração da página de inscrição.

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -2,6 +2,9 @@ import { ApolloClient } from "@apollo/client";
 import { InMemoryCache } from "@apollo/client/cache";
 
 export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.hygraph.com/v2/cl4um1q8j12rh01t7di94dufg/master',
+    uri: import.meta.env.VITE_API_URL,
+    headers: {
+        'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+    },
     cache: new InMemoryCache()
 })
