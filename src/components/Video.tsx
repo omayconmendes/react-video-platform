3c393e0433c011aaf69883c7bfc21a43d0677989
Maycon Mendes <maycon.mendes@senior.com.br>
2023-01-28 12:19:33 -0300

    feat: Instalação codegen e refatoração do código graphql.

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,5 +1,4 @@
 import { Player, Youtube, DefaultUi } from "@vime/react";
-import { gql, useQuery } from "@apollo/client";
 import "@vime/core/themes/default.css";
 import {
   CaretRight,
@@ -8,46 +7,20 @@ import {
   ImageSquare,
   Lightning,
 } from "phosphor-react";
-
-const GET_LESSON_BY_SLUG_QUERY = gql`
-  query GetLessonBySlug($slug: String) {
-    lesson(where: { slug: $slug }) {
-      title
-      videoId
-      teacher {
-        name
-        bio
-        avatarURL
-      }
-    }
-  }
-`;
-
-interface GetLessonBySlugResponse {
-  lesson: {
-    title: string;
-    videoId: string;
-    description: string;
-    teacher: {
-      bio: string;
-      avatarURL: string;
-      name: string;
-    };
-  };
-}
+import { useGetLessonBySlugQuery } from "../graphql/generated";
 
 interface VideoProps {
   lessonSlug: string;
 }
 
 export function Video(props: VideoProps) {
-  const { data } = useQuery(GET_LESSON_BY_SLUG_QUERY, {
+  const { data } = useGetLessonBySlugQuery({
     variables: {
       slug: props.lessonSlug,
     },
   });
 
-  if (!data) {
+  if (!data || !data.lesson) {
     return (
       <div className="flex-1">
         <p>Carregando...</p>;
@@ -74,7 +47,8 @@ export function Video(props: VideoProps) {
             <p className="mt-4 text-grey-200 leading-relaxed">
             {data.lesson.description}
             </p>
-            <div className="flex items-center gap-4 mt-6">
+            {data.lesson.teacher && (
+              <div className="flex items-center gap-4 mt-6">
               <img
                 className="h-16 w-16 rounded-full border-2 border-blue-500"
                 src={data.lesson.teacher.avatarURL}
@@ -89,6 +63,7 @@ export function Video(props: VideoProps) {
                 </span>
               </div>
             </div>
+            )}
           </div>
           <div className="flex flex-col gap-4">
             <a
